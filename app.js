e4bb4d76ca63077044b5b249c42b26cd5f3f38b9
krishanepatel <43988559+krishanepatel@users.noreply.github.com>
2022-03-11 18:15:32 +0000

    commit

--- app.js
+++ app.js
@@ -10,18 +10,35 @@ const mobileMenu = () => {
 menu.addEventListener('click', mobileMenu)
 
 //Collapsible section
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+    } else{
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+    });
+}
 
 //Limit collapsible
-var acc = document.getElementsByClassName("collapsible");
-var panelA = document.querySelectorAll('contentA');
-var panelB = document.querySelectorAll('contentB');
+var acc = document.getElementsByClassName("block");
+var panelA = document.getElementsByClassName('content');
 
 for (var i = 0; i < acc.length; i++) {
     acc[i].onclick = function() {
     	var setClasses = !this.classList.contains('active');
         setClass(acc, 'active', 'remove');
         setClass(panelA, 'show', 'remove');
-        setClass(panelB, 'show', 'remove');
 
        	if (setClasses) {
             this.classList.toggle("active");
@@ -33,5 +50,27 @@ for (var i = 0; i < acc.length; i++) {
 function setClass(els, className, fnName) {
     for (var i = 0; i < els.length; i++) {
         els[i].classList[fnName](className);
+    }
+}
+
+var x = document.getElementsByClassName("content");
+var i;
+var c;
+
+//specify the colors you want to use
+var colors = ["#df6e5c59", "#3d72826c"];
+var d = colors.length;
+
+for (i = 0; i < x.length; i++){
+    while (i < d) {
+        c = i;
+        var random_color = colors[c];
+        x[i].style.background = random_color;
+        i++;
+    }
+    while (i >= d) {
+        var random_color = colors[Math.floor(Math.random() * colors.length)];
+        x[i].style.background = random_color;
+        i++;
     }
 }
