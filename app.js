cf53be29d915db424613f202cd5a03d46dd4607f
krishane patel <krishane.patel@gmail.com>
2023-05-24 22:04:51 +0100

    commit

--- app.js
+++ app.js
@@ -85,3 +85,8 @@ function setClass(els, className, fnName) {
     }
 }
 
+
+import { Gradient } from "https://gist.githack.com/jordienr/64bcf75f8b08641f205bd6a1a0d4ce1d/raw/35a5c7c1ddc9f97ec84fe7e1ab388a3b726db85d/Gradient.js";
+
+const gradient = new Gradient();
+gradient.initGradient("#gradient-canvas");
